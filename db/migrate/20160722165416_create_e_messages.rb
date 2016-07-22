bdcaa02165e70438685f21c8581292b7e752afa7
tomasmaiorino <tomasmaiorino@gmail.com>
2016-07-22 15:58:50 -0300

    add create table script

--- db/migrate/20160722165416_create_e_messages.rb
+++ db/migrate/20160722165416_create_e_messages.rb
@@ -2,7 +2,14 @@ class CreateEMessages < ActiveRecord::Migration
   def change
     create_table :e_messages do |t|
 
-      t.timestamps null: false
+	 t.string :message
+     t.string :subject
+     t.string :sender_name
+     t.string :sender_email
+     t.string :token
+     t.booelan :async, :default => false
+     t.boolean :valid, :default => false
+     t.timestamps null: false
     end
   end
 end
