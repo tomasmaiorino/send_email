4adfbddb29f56b6636f2e5d659564a1dcf201e50
tomasmaiorino <tomasmaiorino@gmail.com>
2016-09-20 23:24:18 -0300

    modifing for postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,63 +13,66 @@
 
 ActiveRecord::Schema.define(version: 20160811022601) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "client_hosts", force: :cascade do |t|
-    t.integer  "client_id",  limit: 4
-    t.string   "host",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "client_id"
+    t.string   "host"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "client_hosts", ["client_id"], name: "index_client_hosts_on_client_id", using: :btree
 
   create_table "client_senders", force: :cascade do |t|
-    t.integer  "client_id",  limit: 4
-    t.integer  "sender_id",  limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.integer  "client_id"
+    t.integer  "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "client_senders", ["client_id"], name: "index_client_senders_on_client_id", using: :btree
   add_index "client_senders", ["sender_id"], name: "index_client_senders_on_sender_id", using: :btree
 
   create_table "clients", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "token",      limit: 255
-    t.boolean  "active",                 default: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.string   "name"
+    t.string   "token"
+    t.boolean  "active",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "e_messages", force: :cascade do |t|
-    t.string   "message",          limit: 255
-    t.string   "code",             limit: 255
-    t.string   "subject",          limit: 255
-    t.string   "sender_name",      limit: 255
-    t.string   "sender_email",     limit: 255
-    t.string   "token",            limit: 255
-    t.boolean  "async",                        default: false
-    t.boolean  "is_message_valid",             default: false
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
+    t.string   "message"
+    t.string   "code"
+    t.string   "subject"
+    t.string   "sender_name"
+    t.string   "sender_email"
+    t.string   "token"
+    t.boolean  "async",            default: false
+    t.boolean  "is_message_valid", default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
   end
 
   create_table "senders", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.boolean  "active",                      default: false
-    t.string   "sender_class",    limit: 255
-    t.string   "additional_data", limit: 255
-    t.string   "send_to",         limit: 255
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
+    t.string   "name"
+    t.boolean  "active",          default: false
+    t.string   "sender_class"
+    t.string   "additional_data"
+    t.string   "send_to"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   create_table "sent_e_messages", force: :cascade do |t|
-    t.string   "status",       limit: 255
-    t.integer  "e_message_id", limit: 4
-    t.integer  "sender_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "message",      limit: 255
+    t.string   "status"
+    t.integer  "e_message_id"
+    t.integer  "sender_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "message"
   end
 
   add_index "sent_e_messages", ["e_message_id"], name: "index_sent_e_messages_on_e_message_id", using: :btree
